a9dede776307505b6d672ca53c879daaa1aae135
Youssef sellami <youssef-sellami@outlook.com>
2021-07-07 20:17:01 +0100

    add the services registration to serviceCollection

---
+++ IdentityServer4.MongoDB/Storage/Configuration/IdentityServerMongoDBBuilderExtensions.cs
@@ -0,0 +1,195 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using IdentityServer4.MongoDB;
+    using IdentityServer4.MongoDB.Database;
+    using IdentityServer4.MongoDB.Options;
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Extension methods to add MongoDB support to IdentityServer.
+    /// </summary>
+    public static class IdentityServerMongoDBBuilderExtensions
+    {
+        /// <summary>
+        /// Adds configuration database to the DI system.
+        /// </summary>
+        /// <param name="services">the services collection</param>
+        /// <param name="storeOptionsAction">The store options action.</param>
+        /// <returns>instance of <see cref="IServiceCollection"/> to enable method chaining</returns>
+        public static IServiceCollection AddConfigurationDatabase(this IServiceCollection services, Action<ConfigurationStoreOptions> storeOptionsAction = null)
+        {
+            // initialize configuration store options and validate it
+            var options = new ConfigurationStoreOptions();
+            storeOptionsAction?.Invoke(options);
+            services.AddSingleton(options);
+            Validate(options);
+
+            // mapping configuration
+            ConfigurationDatabaseAccessor.ConfigureMapping();
+
+            // register the database accessor
+            services.Add<IConfigurationDatabaseAccessor, ConfigurationDatabaseAccessor>(options.RegistrationScope);
+
+            // add the collections configuration
+            services.AddConfigurationDBCollection(options.Client, options.RegistrationScope);
+            services.AddConfigurationDBCollection(options.ApiScope, options.RegistrationScope);
+            services.AddConfigurationDBCollection(options.ApiResource, options.RegistrationScope);
+            services.AddConfigurationDBCollection(options.IdentityResource, options.RegistrationScope);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds operational database to the DI system.
+        /// </summary>
+        /// <param name="services">the services collection</param>
+        /// <param name="storeOptionsAction">The store options action.</param>
+        /// <returns>instance of <see cref="IServiceCollection"/> to enable method chaining</returns>
+        public static IServiceCollection AddOperationalDatabase(this IServiceCollection services, Action<OperationalStoreOptions> storeOptionsAction = null)
+        {
+            // initialize configuration store options and validate it
+            var storeOptions = new OperationalStoreOptions();
+            storeOptionsAction?.Invoke(storeOptions);
+            services.AddSingleton(storeOptions);
+            Validate(storeOptions);
+
+            // mapping configuration
+            PersistedGrantDatabaseAccessor.ConfigureMapping();
+
+            // register the database accessor
+            services.Add<IPersistedGrantDatabaseAccessor, PersistedGrantDatabaseAccessor>(storeOptions.RegistrationScope);
+            services.AddTransient<TokenCleanupService>();
+
+            // add the collections configuration
+            services.AddPersistedGrantDBCollection(storeOptions.DeviceFlowCodes, storeOptions.RegistrationScope);
+            services.AddPersistedGrantDBCollection(storeOptions.PersistedGrant, storeOptions.RegistrationScope);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds an implementation of the IOperationalStoreNotification to the DI system.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddOperationalStoreNotification<T>(this IServiceCollection services)
+           where T : class, IOperationalStoreNotification
+        {
+            services.AddTransient<IOperationalStoreNotification, T>();
+            return services;
+        }
+
+        /// <summary>
+        /// validate the <see cref="ConfigurationStoreOptions "/> instance
+        /// </summary>
+        /// <param name="options">the options to be validated</param>
+        private static void Validate(ConfigurationStoreOptions options)
+        {
+            if (options is null)
+                throw new ArgumentNullException(nameof(options));
+
+            if (options.DatabaseOptions is null)
+                throw new ArgumentNullException(nameof(ConfigurationStoreOptions.DatabaseOptions));
+
+            if (options.DatabaseOptions.MongoClientSettings is null)
+                throw new ArgumentNullException(nameof(ConfigurationStoreOptions.DatabaseOptions.MongoClientSettings));
+
+            if (string.IsNullOrEmpty(options.DatabaseOptions.DatabaseName))
+                throw new ArgumentException("you must provide a valid configuration database name", nameof(ConfigurationStoreOptions.DatabaseOptions.DatabaseName));
+
+            if (options.DatabaseOptions is null)
+                throw new ArgumentNullException(nameof(ConfigurationStoreOptions.DatabaseOptions));
+
+            Validate(options.Client);
+            Validate(options.ApiScope);
+            Validate(options.ApiResource);
+            Validate(options.IdentityResource);
+
+            static void Validate<TDocument>(CollectionConfiguration<TDocument> configuration)
+            {
+                if (configuration is null)
+                    throw new ArgumentNullException($"the collection configuration for {nameof(TDocument)} is null");
+
+                if (string.IsNullOrEmpty(configuration.Name))
+                    throw new ArgumentException($"you must supply a valid collection name for {nameof(TDocument)}");
+            }
+        }
+
+        /// <summary>
+        /// validate the <see cref="ConfigurationStoreOptions "/> instance
+        /// </summary>
+        /// <param name="options">the options to be validated</param>
+        private static void Validate(OperationalStoreOptions options)
+        {
+            if (options is null)
+                throw new ArgumentNullException(nameof(options));
+
+            if (options.DatabaseOptions is null)
+                throw new ArgumentNullException(nameof(ConfigurationStoreOptions.DatabaseOptions));
+
+            if (options.DatabaseOptions.MongoClientSettings is null)
+                throw new ArgumentNullException(nameof(ConfigurationStoreOptions.DatabaseOptions.MongoClientSettings));
+
+            if (string.IsNullOrEmpty(options.DatabaseOptions.DatabaseName))
+                throw new ArgumentException("you must provide a valid Operational database name", nameof(ConfigurationStoreOptions.DatabaseOptions.DatabaseName));
+
+            if (options.DatabaseOptions is null)
+                throw new ArgumentNullException(nameof(ConfigurationStoreOptions.DatabaseOptions));
+
+            Validate(options.PersistedGrant);
+            Validate(options.DeviceFlowCodes);
+
+            static void Validate<TDocument>(CollectionConfiguration<TDocument> configuration)
+            {
+                if (configuration is null)
+                    throw new ArgumentNullException($"the collection configuration for {nameof(TDocument)} is null");
+
+                if (string.IsNullOrEmpty(configuration.Name))
+                    throw new ArgumentException($"you must supply a valid collection name for {nameof(TDocument)}");
+            }
+        }
+
+        /// <summary>
+        /// add a mongodb collection into the Configuration database
+        /// </summary>
+        /// <typeparam name="TDocument">the type of the collection document</typeparam>
+        /// <param name="services">the services instance</param>
+        /// <param name="configuration">the collection configuration</param>
+        /// <param name="scope">the registration scope</param>
+        private static void AddConfigurationDBCollection<TDocument>(this IServiceCollection services, CollectionConfiguration<TDocument> configuration, RegistrationScope scope)
+            => services.Add(scope, provider =>
+            {
+                var centralDatabaseAccessor = provider.GetService<IConfigurationDatabaseAccessor>();
+
+                if (centralDatabaseAccessor is null)
+                    throw new ArgumentNullException("the central Database Accessor is null");
+
+                return centralDatabaseAccessor.Database.GetCollection<TDocument>(configuration.Name, configuration.Settings);
+            });
+
+        /// <summary>
+        /// add a mongodb collection into the Persisted Grant database
+        /// </summary>
+        /// <typeparam name="TDocument">the type of the collection document</typeparam>
+        /// <param name="services">the services instance</param>
+        /// <param name="configuration">the collection configuration</param>
+        /// <param name="scope">the registration scope</param>
+        private static void AddPersistedGrantDBCollection<TDocument>(this IServiceCollection services, CollectionConfiguration<TDocument> configuration, RegistrationScope scope)
+            => services.Add(scope, provider =>
+            {
+                var centralDatabaseAccessor = provider.GetService<IPersistedGrantDatabaseAccessor>();
+                if (centralDatabaseAccessor is null)
+                    throw new ArgumentNullException("the central Database Accessor is null");
+
+                var collection = centralDatabaseAccessor.Database.GetCollection<TDocument>(configuration.Name, configuration.Settings);
+
+                // check if we need to add indexes to the collection
+                if (configuration.Indexes.Any())
+                    collection.Indexes.CreateMany(configuration.Indexes);
+
+                return collection;
+            });
+    }
+}
---
+++ IdentityServer4.MongoDB/Storage/Utilities/ServiceCollectionServiceExtensions.cs
@@ -0,0 +1,138 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using IdentityServer4.MongoDB.Options;
+    using System;
+
+    /// <summary>
+    /// Extension methods for adding services to an Microsoft.Extensions.DependencyInjection.IServiceCollection.
+    /// </summary>
+    public static class ScopeServiceCollectionServiceExtensions
+    {
+        /// <summary>
+        /// Adds a scoped service of the type specified in serviceType to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="scope">the scope to register the service with</param>
+        /// <param name="serviceType">The type of the service to register and the implementation to use.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection Add(this IServiceCollection services, RegistrationScope scope, Type serviceType)
+            => scope switch
+            {
+                RegistrationScope.Transient => services.AddTransient(serviceType),
+                RegistrationScope.Scoped => services.AddScoped(serviceType),
+                RegistrationScope.Singleton => services.AddSingleton(serviceType),
+                _ => services,
+            };
+
+        /// <summary>
+        /// Adds a scoped service of the type specified in serviceType with a factory specified
+        /// in implementationFactory to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="scope">the scope to register with.</param>
+        /// <param name="serviceType">The type of the service to register.</param>
+        /// <param name="implementationFactory">The factory that creates the service.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection Add(this IServiceCollection services, RegistrationScope scope, Type serviceType, Func<IServiceProvider, object> implementationFactory)
+            => scope switch
+            {
+                RegistrationScope.Transient => services.AddTransient(serviceType, implementationFactory),
+                RegistrationScope.Scoped => services.AddScoped(serviceType, implementationFactory),
+                RegistrationScope.Singleton => services.AddSingleton(serviceType, implementationFactory),
+                _ => services,
+            };
+
+        /// <summary>
+        /// Adds a scoped service of the type specified in serviceType with an implementation
+        /// of the type specified in implementationType to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="scope">the scope to register with.</param>
+        /// <param name="serviceType">The type of the service to register.</param>
+        /// <param name="implementationType">The implementation type of the service.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection Add(this IServiceCollection services, RegistrationScope scope, Type serviceType, Type implementationType)
+            => scope switch
+            {
+                RegistrationScope.Transient => services.AddTransient(serviceType, implementationType),
+                RegistrationScope.Scoped => services.AddScoped(serviceType, implementationType),
+                RegistrationScope.Singleton => services.AddSingleton(serviceType, implementationType),
+                _ => services,
+            };
+
+        /// <summary>
+        /// Adds a scoped service of the type specified in TService to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="scope">the scope to register with.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection Add<TService>(this IServiceCollection services, RegistrationScope scope) where TService : class
+            => scope switch
+            {
+                RegistrationScope.Transient => services.AddTransient<TService>(),
+                RegistrationScope.Scoped => services.AddScoped<TService>(),
+                RegistrationScope.Singleton => services.AddSingleton<TService>(),
+                _ => services,
+            };
+
+        /// <summary>
+        /// Adds a scoped service of the type specified in TService with a factory specified
+        /// in implementationFactory to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="scope">the scope to register with.</param>
+        /// <param name="implementationFactory">The factory that creates the service.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection Add<TService>(this IServiceCollection services, RegistrationScope scope, Func<IServiceProvider, TService> implementationFactory)
+            where TService : class => scope switch
+            {
+                RegistrationScope.Transient => services.AddTransient(implementationFactory),
+                RegistrationScope.Scoped => services.AddScoped(implementationFactory),
+                RegistrationScope.Singleton => services.AddSingleton(implementationFactory),
+                _ => services,
+            };
+
+        /// <summary>
+        /// Adds a scoped service of the type specified in TService with an implementation
+        /// type specified in TImplementation to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="scope">the scope to register with.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection Add<TService, TImplementation>(this IServiceCollection services, RegistrationScope scope)
+            where TService : class
+            where TImplementation : class, TService => scope switch
+            {
+                RegistrationScope.Transient => services.AddTransient<TService, TImplementation>(),
+                RegistrationScope.Scoped => services.AddScoped<TService, TImplementation>(),
+                RegistrationScope.Singleton => services.AddSingleton<TService, TImplementation>(),
+                _ => services,
+            };
+
+        /// <summary>
+        /// Adds a scoped service of the type specified in TService with an implementation
+        /// type specified in TImplementation using the factory specified in implementationFactory
+        /// to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service to add.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation to use.</typeparam>
+        /// <param name="services">The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service to.</param>
+        /// <param name="scope">the scope to register with</param>
+        /// <param name="implementationFactory">The factory that creates the service.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection Add<TService, TImplementation>(this IServiceCollection services, RegistrationScope scope, Func<IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService => scope switch
+            {
+                RegistrationScope.Transient => services.AddTransient(implementationFactory),
+                RegistrationScope.Scoped => services.AddScoped(implementationFactory),
+                RegistrationScope.Singleton => services.AddSingleton(implementationFactory),
+                _ => services,
+            };
+
+    }
+}
