a07d955a45cae91db45911df0ab6ed65d4fe9080
Youssef sellami <youssef-sellami@outlook.com>
2021-07-07 20:22:26 +0100

    added the extensions methods over the IIdentityServerBuilder to add Mongodb support

---
+++ IdentityServer4.MongoDB/IdentityServerEntityFrameworkBuilderExtensions.cs
@@ -0,0 +1,86 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    using IdentityServer4.MongoDB;
+    using IdentityServer4.MongoDB.Options;
+    using IdentityServer4.MongoDB.Services;
+    using IdentityServer4.MongoDB.Stores;
+    using IdentityServer4.Stores;
+    using Microsoft.Extensions.Hosting;
+    using System;
+
+    /// <summary>
+    /// Extension methods to add MongoDb database support to IdentityServer.
+    /// </summary>
+    public static class IdentityServerEntityFrameworkBuilderExtensions
+    {
+        /// <summary>
+        /// Configures MongoDb implementation of IClientStore, IResourceStore, and ICorsPolicyService with IdentityServer.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="storeOptionsAction">The store options action.</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddConfigurationStore(
+            this IIdentityServerBuilder builder,
+            Action<ConfigurationStoreOptions> storeOptionsAction = null)
+        {
+            builder.Services.AddConfigurationDatabase(storeOptionsAction);
+
+            builder.AddClientStore<ClientStore>();
+            builder.AddResourceStore<ResourceStore>();
+            builder.AddCorsPolicyService<CorsPolicyService>();
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Configures caching for IClientStore, IResourceStore, and ICorsPolicyService with IdentityServer.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddConfigurationStoreCache(
+            this IIdentityServerBuilder builder)
+        {
+            builder.AddInMemoryCaching();
+
+            // add the caching decorators
+            builder.AddClientStoreCache<ClientStore>();
+            builder.AddResourceStoreCache<ResourceStore>();
+            builder.AddCorsPolicyCache<CorsPolicyService>();
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Configures MongoDb implementation of IPersistedGrantStore with IdentityServer.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="storeOptionsAction">The store options action.</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddOperationalStore(
+            this IIdentityServerBuilder builder,
+            Action<OperationalStoreOptions> storeOptionsAction = null)
+        {
+            builder.Services.AddOperationalDatabase(storeOptionsAction);
+
+            builder.Services.AddTransient<IPersistedGrantStore, PersistedGrantStore>();
+            builder.Services.AddTransient<IDeviceFlowStore, DeviceFlowStore>();
+            builder.Services.AddSingleton<IHostedService, TokenCleanupHost>();
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds an implementation of the IOperationalStoreNotification to IdentityServer.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddOperationalStoreNotification<T>(
+           this IIdentityServerBuilder builder)
+           where T : class, IOperationalStoreNotification
+        {
+            builder.Services.AddOperationalStoreNotification<T>();
+            return builder;
+        }
+    }
+}
