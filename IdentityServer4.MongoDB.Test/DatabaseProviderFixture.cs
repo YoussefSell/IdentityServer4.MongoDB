a95018c1af2e2c4beaf28df34f2a309011dd19b3
Youssef sellami <youssef-sellami@outlook.com>
2021-07-07 20:49:10 +0100

    installed required packages and added the base test classes

---
+++ IdentityServer4.MongoDB.Test/DatabaseProviderFixture.cs
@@ -0,0 +1,22 @@
+﻿using Mongo2Go;
+using System;
+
+namespace IdentityServer4.MongoDB.Test
+{
+    public class MongoDatabaseFixture : IDisposable
+    {
+        public MongoDbRunner Runner => _runner;
+        private MongoDbRunner _runner;
+
+        public MongoDatabaseFixture()
+        {
+            _runner = MongoDbRunner.Start();
+        }
+
+        public void Dispose()
+        {
+            _runner.Dispose();
+            _runner = null;
+        }
+    }
+}
